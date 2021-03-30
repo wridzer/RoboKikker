af2f2bbc4081ccf94bc024297035c6b582a03ddf
Wridzer <wridzer12@gmail.com>
2021-03-30 21:25:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ bot.on('ready', () => {
 bot.login(TOKEN);
 
 bot.on('message', msg => {
-  @client.event
-  async def on_message(msg):
-  if (msg.author == bot.user){return}
-  if (msg.author.bot){return}
+  //@client.event
+  //async def on_message(msg):
+  //if (msg.author == bot.user){return}
+  //if (msg.author.bot){return}
   if (msg.content === 'maar echt') {
-      msg.channel.send('maar echt');
+      msg.channel.send('maar echt!');
   } else if (msg.content.startsWith('!report')) {
     if (msg.mentions.users.size) {
       const taggedUser = msg.mentions.users.first();
