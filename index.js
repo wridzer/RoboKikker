c0e6958ba2ee11b12af7ec7e62fe67aad354d4e2
Wridzer <wridzer12@gmail.com>
2021-03-30 18:18:09 +0200

    config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN;
+const TOKEN = require('./config.json');
 
 bot.login(TOKEN);
 
