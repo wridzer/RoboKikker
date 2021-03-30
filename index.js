896cdf6d0c10954faf648c29fde4a37329a8db91
Wridzer <wridzer12@gmail.com>
2021-03-30 22:36:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const TOKEN = require('./config.json');
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
+const port = process.env.PORT || 3000;
 
 bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
