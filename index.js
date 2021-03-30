749265c434279d08b0e7e20c641007c7ad6e928c
Wridzer <wridzer12@gmail.com>
2021-03-30 22:02:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ bot.on('ready', () => {
 
 bot.login(TOKEN.token);
 
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
+
 bot.on('message', msg => {
   //@client.event
   //async def on_message(msg):
