1f71a980add5d9984bc9fb23f6e42bdcae7157c6
Wridzer <wridzer12@gmail.com>
2021-03-30 21:05:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-bot.login(TOKEN);
+bot.login("ODI2MTkzMzAxODc4MzQxNjYy.YGI6eQ.sV4z1rnGba0Mrrry69J9Ec2Gi58");
 
 bot.on('message', msg => {
   if (msg.author == client.user){return}
