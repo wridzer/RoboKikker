c3438e743561b970ad9ad9ec15656366bb1f76a6
Wridzer <wridzer12@gmail.com>
2021-03-30 19:02:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ bot.on('message', msg => {
   if (msg.content === 'lit') {
     msg.channel.send('maar echt');
 
-  } else if (msg.content.startsWith('!kick')) {
+  } else if (msg.content.startsWith('!report')) {
     if (msg.mentions.users.size) {
       const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+      msg.channel.send(`You wanted to report: ${taggedUser.username}`);
     } else {
       msg.reply('Please tag a valid user!');
     }
