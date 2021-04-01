26280f9bdc25a6cd1d1722194c5a23e9666388f9
Wridzer <wridzer12@gmail.com>
2021-04-02 01:04:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ bot.on('message', msg => {
     if (msg.mentions.users.size) {
       const taggedUser = msg.mentions.users.first();
       msg.channel.send(`You wanted to report for simping: ${taggedUser.username}`);
-      mikeznstarfhoek = client.get_channel(#776124955988590593);
-      taggedUser.voice.setChannel(mikeznstarfhoek);
+      //mikeznstarfhoek = client.get_channel(#776124955988590593);
+      taggedUser.voice.setChannel('776124955988590593');
     } else {
       msg.reply('Please tag a valid user!');
     }
