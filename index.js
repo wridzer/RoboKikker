4cdb3c0fbe01a80e41ddf2289a09efee872fdb54
Wridzer <wridzer12@gmail.com>
2021-04-05 00:08:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,8 @@ bot.on('message', msg => {
         msg.delete(1000);
         msg.author.send(`Fuck off! :middle_finger:`);
       } else {
-      taggedUser.send(msg.content);
+      message1 = msg.content.substring(10+taggedUser.nickname.length, msg.content.length)
+      taggedUser.send(message1);
       msg.delete(1000);
       }
     } else {
