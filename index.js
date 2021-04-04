dc7def28b4ec32595fd5039cfea7e8802488c916
Wridzer <wridzer12@gmail.com>
2021-04-04 23:22:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ bot.on('message', msg => {
       msg.reply('Please tag a valid user!');
     }
   }
-  if (!msg.content.startsWith('\"') && msg.channel.id == 768120241308958800)
+  if (!msg.content.startsWith('\"') || msg.attachments.size > 0 && msg.channel.id == 768120241308958800)
   {
     msg.delete(1000);
-    msg.author.send(`Please reserve this channel for quotes`);
+    msg.author.send(`this is not a quote, you freakin pancake`);
   }
 });
