4c43a9b379e231f884c21a876601e80012dd38db
Wridzer <wridzer12@gmail.com>
2021-04-13 17:33:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -145,6 +145,6 @@ bot.on('message', msg => {
 bot.on('messageReactionAdd', (reaction, user) => {
     if(reaction.emoji.name === "✔️")
     {
-        reaction.users.roles.addRole(role);
+        reaction.users.guild.roles.add(role);
     }
 });
