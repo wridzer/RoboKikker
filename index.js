718fa9b816d058b5794f867a644075ec88ccbb96
Wridzer <wridzer12@gmail.com>
2021-04-14 19:03:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -147,8 +147,8 @@ bot.on('messageReactionAdd', (reaction, user) => {
     {
         //user.roles.add('831547671110090774');
         //let server = bot.guilds.cache.get('757737687921852496')
-        var memberRole= server.roles.cache.find(role => role.name === "ArTisT")
-        //let role = reaction.message.guild.roles.find(role => role.id == 831547671110090774);
+        //var memberRole= server.roles.cache.find(role => role.name === "ArTisT")
+        let role = reaction.message.guild.roles.find(role => role.id == 831547671110090774);
         //if (!role) return;
         console.log(role.id);
         reaction.message.guild.member.roles.add(role).catch((error) => {
