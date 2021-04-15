93266178feb9b8702e39029307067f14a64cd2f3
Wridzer <wridzer12@gmail.com>
2021-04-15 02:39:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ bot.on('raw', packet => {
     //const channel = bot.channels.cache.get(packet.d.channel_id);
     const channel = bot.guilds.get('757737687921852496').channels.get(packet.d.channel_id);
     console.log(channel.messages);
-    channel.messages.fetch(packet.d.message_id).then(message => {
+    channel.messages.get(packet.d.message_id).then(message => {
         const servers = bot.guilds.cache.get(packet.d.guild_id);
         if (packet.t === 'MESSAGE_REACTION_ADD'){
             if(packet.d.emoji.name === "✔️"){
