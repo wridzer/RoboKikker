b2bfb9e28b8022e889840cd4a125d9de1090559f
Wridzer <wridzer12@gmail.com>
2021-04-15 03:05:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 ﻿require('dotenv').config();
 const Discord = require('discord.js');
-const bot = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+const bot = new Discord.Client(/*{
+    partials: ["REACTION", "MESSAGE"],
+    ws: {
+        intents: ["GUILD_MEMBERS", "GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"]
+    }
+}*/);
 const TOKEN = require('./config.json');
 
 const port = process.env.PORT || 5000;
-
-//var classreps = ["220190663780728842", "200954723962978304", "402552605106241546", "319156673916960769", "489711674815938560"];
-
-//var isSend;
 
 var initialized = false;
 
@@ -151,25 +152,6 @@ bot.on('message', msg => {
   }
 });
 
-bot.on('messageReactionAdd', async (reaction, user) => {
-    // When we receive a reaction we check if the reaction is partial or not
-    if (reaction.partial) {
-        // If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
-        try {
-            await reaction.fetch();
-        } catch (error) {
-            console.error('Something went wrong when fetching the message: ', error);
-            // Return as `reaction.message.author` may be undefined/null
-            return;
-        }
-    }
-    // Now the message has been cached and is fully available
-    console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
-    // The reaction is now also fully available and the properties will be reflected accurately:
-    console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
-});
-
-/*
 bot.on('raw', packet => {
     if (!initialized) return;
     if (!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) return;
@@ -189,7 +171,7 @@ bot.on('raw', packet => {
         });
     });
 });
-*/
+
 /*
 bot.on('messageReactionAdd', (reaction, user) => {
     if(reaction.emoji.name === "✔️")
